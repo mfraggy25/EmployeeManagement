6c168328769a2bfc1d27e8b4a3cbfd2271473c9b
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-07-05 16:00:59 -0400

    amend alerts

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -49,7 +49,7 @@ export default {
         .then(
           user => {
             // console.log(user);
-            alert(`Account Created for ${user.email}`);
+            alert(`Account Created!`);
             this.$router.go({ path: this.$router.path });
           },
           err => {
