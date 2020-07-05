b18e60012b1ced62d09a1a36b06536b017509985
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-07-05 12:48:55 -0400

    First commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCdOTLXSR_Qw5PaMU3H7uukqrK1642tsUM",
+  authDomain: "vueproject-a883a.firebaseapp.com",
+  databaseURL: "https://vueproject-a883a.firebaseio.com",
+  projectId: "vueproject-a883a",
+  storageBucket: "vueproject-a883a.appspot.com",
+  messagingSenderId: "944946415889",
+  appId: "1:944946415889:web:770fd44e62e75539f064b0",
+  measurementId: "G-0VFJBPKBMG"
+};
